c6294ee80f488a283c4c48224769b75ff3c0b7a1
Elias <elmoreno@uveg.edu.mx>
2023-11-04 09:57:11 -0600

    Agregar archivos de proyecto.

---
+++ RazorPageMovie/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPageMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        //Se debe de poner esta línea cuando se requiere utiliza el tipo de dato Date
+        [Display(Name ="Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime RelaseDate { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
+        public string? Genre { get; set; }
+        [Column(TypeName = "decimal(18:2)")]
+        public decimal Price { get; set; }
+        public string? Rating { get; set; }
+        //[RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        //[Required]
+        
+    }
+}
---
+++ RazorPageMovie/Models/SeedData.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPageMovie.Data;
+
+namespace RazorPageMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider) 
+        {
+            using (var context = new RazorPageMovieContext1(
+                serviceProvider.GetRequiredService<
+                DbContextOptions<RazorPageMovieContext1>>()))
+            {
+                //Se verifica de que el context es null, se envia mensaje de error
+                if(context == null || context.Movie == null)
+                {
+                    throw new ArgumentNullException("Null RazorPAgeMovieContext");
+                }
+                if(context.Movie.Any())
+                {
+                    // La BD retorna o muestra todo lo que contiene esta clase
+                    return;
+                }
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When HArry met Sally",
+                        RelaseDate = DateTime.Parse("1989-2-12"),
+                        Genre="Romantic Comedy",
+                        Price = 7.99M,
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "Goshbusters",
+                        RelaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M,
+                        Rating = "G"
+                    },
+                    new Movie
+                    {
+                        Title = "Goshbusters 2",
+                        RelaseDate = DateTime.Parse("1989-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M,
+                        Rating = "G"
+                    },
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        RelaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M,
+                        Rating = "NA"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
